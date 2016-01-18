e8e1e10cbed566a591eb01725ef81bbbbe748f3f
Jordaen Graham <jordaen.graham@workiva.com>
2016-01-18 08:56:08 -0600

    A3 initial commit

---
+++ server.c
@@ -0,0 +1,13 @@
+/* CMPT 434 - Winter 2016
+ * Assignment 1, Question 3
+ *
+ * Jordaen Graham - jhg257
+ *
+ * File: server.c
+ */
+
+#include "server.h"
+
+int main(int argc, char **argv){
+    return 0;
+}
---
+++ server.h
@@ -0,0 +1,29 @@
+/* CMPT 434 - Winter 2016
+ * Assignment 1, Question 3
+ *
+ * Jordaen Graham - jhg257
+ *
+ * File: server.h
+ */
+
+
+#ifndef CMPT434_SERVER_H
+#define CMPT434_SERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MYPORT "30950"	// the port users will be connecting to
+
+#define MAXBUFLEN 100
+
+
+#endif //CMPT434_SERVER_H
